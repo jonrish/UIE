58a3640847dde5e4d4de1e8c1fd751e8ce08aadc
Jon Rish <jon.m.rish@gmail.com>
2013-08-11 11:55:02 -0400

    style form / experts h3 & h4 / footer highlight js

--- ui18_giveaway_1/ui18_giveaway_1.js
+++ ui18_giveaway_1/ui18_giveaway_1.js
@@ -5,3 +5,13 @@ $('ul.experts-list').on('mouseenter', 'li', function() {
 		$this.removeClass('highlight-experts')
 	});
 });
+
+$('footer').on('mouseenter', 'a', function() {
+	var $this = $(this);
+	$this.addClass('highlight-link');
+	$this.on('mouseleave', function() {
+		$this.removeClass('highlight-link')
+	});
+})
+
+
