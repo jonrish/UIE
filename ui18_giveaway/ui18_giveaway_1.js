56513eca34269ec9b7b4339139114993cd331b3f
Jon Rish <jon.m.rish@gmail.com>
2013-08-12 19:42:48 -0400

    refactor js into re-usable function

--- ui18_giveaway/ui18_giveaway_1.js
+++ ui18_giveaway/ui18_giveaway_1.js
@@ -1,17 +1,14 @@
-$('ul.experts-list').on('mouseenter', 'li', function() {
-	var $this = $(this);
-	$this.addClass('highlight-experts');
-	$this.on('mouseleave', function() {
-		$this.removeClass('highlight-experts')
+(function() {
+	var experts = new HighlightClass($('ul.experts-list'), 'li', 'highlight-experts');
+	var link = new HighlightClass($('footer'), 'a', 'highlight-link');
+})();
+
+function HighlightClass(parent_element, target_element, target_class) {
+	$(parent_element).on('mouseenter', target_element, function() {
+		var $this = $(this);
+		$this.addClass(target_class);
+		$this.on('mouseleave', function() {
+			$this.removeClass(target_class);
+		});
 	});
-});
-
-$('footer').on('mouseenter', 'a', function() {
-	var $this = $(this);
-	$this.addClass('highlight-link');
-	$this.on('mouseleave', function() {
-		$this.removeClass('highlight-link')
-	});
-})
-
-
+};
